3c7d6fd3195755377c0c883311b18088c170811c
Tallons <Tallons27@yahoo.com>
2021-06-26 23:33:23 -0600

    Day7 Start

---
+++ Day7/index.js
@@ -0,0 +1,30 @@
+const worldAthletes = [
+    {first: "Mikhail", last: "Zalomin", country: "Russia", pass1: 37.1, pass2: 38.9},
+    {first: "Ruban", last: "Padilla", country: "United States", pass1: 37.6, pass2: 38.4},
+    {first: "Vasilii", last: "Makarskii", country: "Russia", pass1: 37.1, pass2: 38.6},
+    {first: "Andres", last: "Martinez", country: "Spain", pass1: 36.2 , pass2: 37.8},
+    {first: "Mario", last: "Bruno", country: "Canada", pass1: 36.8 , pass2: 37.1},
+    {first: "Alexander", last: "Renkert", country: "United States", pass1: 37.7 , pass2: 36.1},
+    {first: "Tiago", last: "Sampaio", country: "Portugal", pass1: 36.6 , pass2: 37.2},
+    {first: "Andrei", last:"", country: "Russia", pass1: 36.7 , pass2: 37.0},
+    {first: "Carlos", last: "Manjon", country: "Spain", pass1: 36.9 , pass2: 36.3},
+    {first: "Joao", last: "Caeiro", country: "Portugal", pass1: 36.3 , pass2: 36.8},
+    {first: "Daniel", last: "Berridge", country: "Great Britain", pass1: 36.7 , pass2: 36.4},
+    {first: "Simon", last: "Smith", country: "United States", pass1: 36.5 , pass2: 36.5},
+    {first: "Alejando", last: "Bernardez", country: "Spain", pass1: 37.5 , pass2: 35.3},
+    {first: "Lucas", last: "Adorno", country: "Argentina", pass1: 36.2 , pass2: 36.5},
+    {first: "Diogo", last: "Carvalho", country: "Portugal", pass1: 34.6 , pass2: 38.0},
+    {first: "Rhys", last: "Gray", country: "Great Britain", pass1: 37.1 , pass2: 35.3},
+    {first: "Jones", last: "Nordfors", country: "Sweden", pass1: 36.9 , pass2: 35.4},
+    {first: "Daniel", last: "Schmidt", country: "Germany", pass1: 36.9 , pass2: 35.3},
+    {first: "Daniel", last: "Perez", country: "Spain", pass1: 36.5 , pass2: 35.2},
+    {first: "Noah", last: "Orr", country: "United States", pass1: 36.2 , pass2: 35.1}
+]
+
+// Array.some() - is at least one score over 38.0?
+// Array.every() - does everyone have at least one score over a 36.0
+
+// Array.find() - find Simon
+
+// Array.findIndex() - find the index of Alexander
+// Array.delete() - delete Daniel Perez
